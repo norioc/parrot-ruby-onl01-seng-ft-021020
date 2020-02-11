6ada269b076723610db6ef56f54bd8177f099865
decisive-reducer-5526 <norionorioc@gmail.com>
2020-02-11 02:43:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name="Pretty bird!")
-  
-  puts "Squawk!"
-  "Pretty bird!"
-  "Squawk!"
-end 
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+  return phrase
+
+end
